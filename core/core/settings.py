e1ad3a379d739028b1c6c0f98e4bb122eea1a8ee
Miguel Cadeiras <miguelcadeiras@gmail.com>
2021-04-16 06:28:38 -0300

    collectstatic

--- core/core/settings.py
+++ core/core/settings.py
@@ -26,7 +26,7 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = '67xtp3x=wm2w4$y*xa2w9)#0%-0%o$@totaqn%mdr38f=48!se'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'),'*']
 ALLOWED_HOSTS = ['*']
