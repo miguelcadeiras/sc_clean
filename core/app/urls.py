63f44b178544816d4145e36f83703063562e6c9f
miguelcadeiras@gmail.com <miguelcadeiras@gmail.com>
2021-11-11 17:28:42 -0300

    devices

--- core/app/urls.py
+++ core/app/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
          name="password_reset_complete"),
     path('status', views.status, name='status'),
-    path('devices', views.devices, name='devices'),
+    path('devices', views.devices, name='devices') ,
 
 ]
