9b5df363de02769e59f822a58dc8aa5356ffda34
miguelcadeiras <7Angelito9>
2022-07-02 09:25:55 -0300

    plus minus. back next list disable.. URI TOO LONG

--- core/app/views.py
+++ core/app/views.py
@@ -1713,7 +1713,7 @@ def plusMinus(request):
     id_inspection = request.GET['id_inspection']
     id_unit = request.GET['id_unit'].strip()
     # falseList=request.GET['list']
-    falseList=[0]
+    falseList=[]
     agvPos = request.GET['agvPos'].strip()
     data = []
     dfw = pd.DataFrame
