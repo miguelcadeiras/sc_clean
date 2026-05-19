42759f1c8e16cd06a50701b0a5c6be23ee50217f
miguelcadeiras <7Angelito9>
2026-05-19 15:36:49 -0300

    Add v2 readed analysis route

--- core/app_v2/urls.py
+++ core/app_v2/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from app import views as legacy_views
 from . import views
 
 urlpatterns = [
     path('all', views.all_vr_no_pd_v2, name='all_v2'),
+    path('readedAnalysis', legacy_views.readedAnalysisVR_noPD, name='readed_analysis_v2'),
 ]
